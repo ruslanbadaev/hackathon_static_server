59963dd4619726e9b0459447545986912a2e377c
Ruslan Badaev <r.badaeff@gmail.com>
2022-03-20 14:49:33 +0300

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+  path = require('path'),
+  app = express();
+app.set('port', 80);
+
+app.use(express.static(path.join(__dirname, './public')));
+
+var server = app.listen(app.get('port'), function () {
+  console.log('The server is running on http://localhost:' + app.get('port'));
+});
